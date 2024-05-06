5131ee562705a1791160deebd8af32bc3b70a304
Vinicius <viniss-starwars@hotmail.com>
2024-05-05 21:09:29 -0300

    arrumando footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,9 @@
 import {} from 'react';
+import { FaSquareFacebook } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+
 
 
 function Footer() {
@@ -6,7 +11,13 @@ function Footer() {
   return (
     <>
     <container className="footerContainer">
-       <h5 className='footer'>@2024-todos os direitos reservados.</h5>
+      <h5 className='footer'>@2024-todos os direitos reservados.</h5>
+      <div className='div-icons'>
+        <a href="https://www.facebook.com/"><FaSquareFacebook className='icons'/></a>
+        <a href="https://www.instagram.com/"><FaInstagram className='icons'/></a>
+        <a href="https://linkedin.com"><FaLinkedin className='icons'/></a>
+        <a href="https://gmail.com"><MdEmail className='icons'/></a>
+      </div>
     </container>
     </>
   );
